468a67f9b1d0cb70b131d8fabc374e39cb4341ca
Pedro Henrique <pedroogsch@gmail.com>
2025-11-11 11:24:45 -0300

    fix: Força a leitura da VITE_API_URL no App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ import {
 
 import EstoqueChart from './EstoqueChart' 
 
-const API_URL = 'http://127.0.0.1:8000'
+const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000';
 
 function App() {
   // --- ESTADOS ---
